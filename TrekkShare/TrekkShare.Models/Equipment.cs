7110a7c304c8b23248705703596a375d294938e6
vili <aleksandrova.v@gmail.com>
2019-03-03 16:23:14 +0200

    Equipment model added

---
+++ TrekkShare/TrekkShare.Models/Equipment.cs
@@ -0,0 +1,15 @@
+﻿
+namespace TrekkShare.Models
+{
+    using TrekkShare.Models.Enums.RouteEnums;
+
+
+    public class Equipment
+    {
+        public int EquipmentId { get; set; }
+                
+        public string Name { get; set; }
+
+        public EquipmentLevel EquipmentLevel { get; set; }
+    }
+}
